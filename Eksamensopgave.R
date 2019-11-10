c4b076f9b57bfa5c07446fb674d259308db066ec
Burhan27 <s175446@student.dtu.dk>
2019-11-10 17:36:32 +0100

    Plot3d for bydele i 2012

--- Eksamensopgave.R
+++ Eksamensopgave.R
@@ -1,57 +1,28 @@
-kbh <- read.csv("befkbhalderstatkode.csv")
+kbhbefolkning <- read.csv("befkbhalderstatkode.csv")
 
 # fjern observationer med manglende værdier
 # Data uden etniske danskere
-ikkeDanskData <-subset(kbh,(STATKODE !=0) & (STATKODE !=5001) & (STATKODE !=5100) & (STATKODE !=5199) & (STATKODE !=5471) & (STATKODE != 5499) & (STATKODE !=5800) & (STATKODE !=5901) & (STATKODE !=5999))
+kbhIkkeDansk <-subset(kbhbefolkning,(STATKODE !=0) & (STATKODE !=5001) & (STATKODE !=5100) & (STATKODE !=5199) & (STATKODE !=5471) & (STATKODE != 5499) & (STATKODE !=5800) & (STATKODE !=5901) & (STATKODE !=5999))
 
 #Data med hele befolkningen i kbh
-kbhdata <- subset(kbh,(STATKODE !=0) & (STATKODE !=5001) & (STATKODE !=5199) & (STATKODE !=5471) & (STATKODE != 5499) & (STATKODE !=5800) & (STATKODE !=5901) & (STATKODE !=5999))
+kbhUStatsløse <- subset(kbhbefolkning,(STATKODE !=0) & (STATKODE !=5001) & (STATKODE !=5199) & (STATKODE !=5471) & (STATKODE != 5499) & (STATKODE !=5800) & (STATKODE !=5901) & (STATKODE !=5999))
 
 #kun etniske danskkbh
-danskData <- subset(kbh,(STATKODE == 5100))
+kbhDanskere <- subset(kbhbefolkning,(STATKODE == 5100))
 
-kbhdataSub <- subset(kbh,(AAR == 2012))
+kbh2012 <- subset(kbhbefolkning,(AAR == 2012))
 
 #graf for 2012 lagt sammen i alder
-df2012 = data.frame( kbhdataSub$ALDER, kbhdataSub$PERSONER)
 
-nigga <- ddply(df2012,.(kbhdataSub.ALDER),summarize,sum=sum(kbhdataSub.PERSONER),number=length(kbhdataSub.ALDER))
+df2012 = data.frame(kbh2012$BYDEL, kbh2012$ALDER, kbh2012$PERSONER)
+names(df2012) <-  c('BYDEL','ALDER','PERSONER')
 
-plot(nigga$kbhdataSub.ALDER,nigga$sum)
+df2012v1 = subset(df2012,(BYDEL !=99))
 
+names(df2012v1) <-  c('BYDEL','ALDER','PERSONER')
 
+ss2012 <- ddply(df2012v1,.(ALDER, BYDEL),summarize,sum=sum(PERSONER),number=length(ALDER))
 
-#plotter alderen over frekvensen
-plot(slogSam$kbhdataSub.ALDER, slogSam$V1)
+attach(ss2012)
 
-#ounter = subset(testdataframe,(ALDER == )) 
-
-#plot(kbhdataSub$ALDER,kbhdataSub$PERSONER,type = "1",col="red")
-
-
-#Gøre det her op til alderen 100 i 10 år per diagram
-######################################################################################
-#danskere mellem 0-30 alderen i 2012
-kbh030 <- subset(kbhdataSub,(ALDER < 30))
-
-kbh3060 <- subset(kbhdataSub, (ALDER > 29) & (ALDER < 60))
-#ikke danskere mellem 0-10 i hele datasættets forløb
-
-kbh60100 <- subset(kbhdataSub,(ALDER > 60) & (ALDER < 100))
-
-######################################################################################
-#ikke danskere mellem 10-19 alderen
-ikkeDansk919 <- subset(ikkeDanskData,(ALDER > 9) & (ALDER < 20))
-
-#ikke danskere mellem 0-10 i hele datasættets forløb
-hist(ikkeDansk919$AAR, xlab = "Årgang", main = "Mennesker mellem alderen fra 0 - 10")
-######################################################################################
-hist(danskData$AAR)
-
-hist(ikkeDanskData$ALDER)
-
-hist(ikkeDanskData$PERSONER)
-
-#plot3d(ikkeDanskData$STATKODE,ikkeDanskData$PERSONER,ikkeDanskData$ALDER, main="Basic 3D Scatter Plot")
-
-hist(ikkeDanskData$ALDER, xlab = "Alder", main = "Histogram af ikke danskeres aldre")
+plot3d(ss2012$ALDER, ss2012$BYDEL, ss2012$sum)
